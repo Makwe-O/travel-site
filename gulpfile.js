6fae819da2a366d5f74272a18e4674d9ceae1c64
Mmakwe Onyeka <notmaks@yahoo.com>
2018-03-01 23:15:03 +0100

    Added postcss automation

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,30 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const watch = require('gulp-watch');
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+const cssvars = require('postcss-simple-vars');
+const nested = require('postcss-nested');
+
+gulp.task('default', () =>{
+  console.log("You created your first task!");
+});
+
+gulp.task('html', () =>{
+  console.log("Hello")
+})
+
+gulp.task('styles', () =>{
+    return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([autoprefixer(), cssvars(), nested()]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', () =>{
+  watch('./app/index.html', () =>{
+    gulp.start('html');
+  });
+  watch('./app/assets/styles/**/*.css', () =>{
+    gulp.start('styles');
+  });
+
+});

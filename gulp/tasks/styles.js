08d4efda07ef09d5574dc67b0a17fdf02847282d
Mmakwe Onyeka <notmaks@yahoo.com>
2018-03-15 20:17:42 +0100

    Added mixins

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -9,5 +9,9 @@ const atImport = require('postcss-import');
 gulp.task('styles', () =>{
     return gulp.src('./app/assets/styles/styles.css')
     .pipe(postcss([atImport(),  autoprefixer(), cssvars(), nested()]))
+    .on('error',  function(errInfo){
+      console.log(errInfo.toString());
+      this.emit('end');
+    })
     .pipe(gulp.dest('./app/temp/styles'));
 });

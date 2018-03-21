d0568b6b34e4c01d62a69b3373d99d6aca755945
Mmakwe Onyeka <notmaks@yahoo.com>
2018-03-21 13:14:42 +0100

    Added responsive images to the banner

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@ const autoprefixer = require('autoprefixer');
 const cssvars = require('postcss-simple-vars');
 const nested = require('postcss-nested');
 const atImport = require('postcss-import');
+const mixins = require('postcss-mixins');
 
 
 gulp.task('styles', () =>{
     return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([atImport(),  autoprefixer(), cssvars(), nested()]))
+    .pipe(postcss([atImport(), mixins(),  autoprefixer(), cssvars(), nested()]))
     .on('error',  function(errInfo){
       console.log(errInfo.toString());
       this.emit('end');

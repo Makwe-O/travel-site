2e8a41a2e31c6c7ad1b325779f3f372a9f8f0be9
Mmakwe Onyeka <notmaks@yahoo.com>
2018-04-21 00:59:25 +0100

    Added sprite package

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,1 +1,21 @@
 const gulp = require('gulp');
+const svgSprite = require('gulp-svg-sprite');
+
+const config = {
+  mode: {
+    css:{
+      render:{
+        css: {
+              template: './gulp/templates/sprite.css'
+        }
+
+      }
+    }
+  }
+}
+
+gulp.task('createSprite', ()=>{
+  return gulp.src('./app/assets/images/icons/**/*.svg')
+  .pipe(svgSprite(config))
+  .pipe(gulp.dest('./app/temp/sprite'));
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,3 @@
 require('./gulp/tasks/styles');
 require('./gulp/tasks/watch');
-require('./gulp/tasks/sprite');
+require('./gulp/tasks/sprites');

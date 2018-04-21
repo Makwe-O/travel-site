fff7d0e0f4a4d74e4dd298d0f17279b123811194
Mmakwe Onyeka <notmaks@yahoo.com>
2018-04-21 01:16:39 +0100

    Added rename package and moved sprite css to main css module

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const svgSprite = require('gulp-svg-sprite');
+const rename = require('gulp-rename');
 
 const config = {
   mode: {
@@ -16,6 +17,12 @@ const config = {
 
 gulp.task('createSprite', ()=>{
   return gulp.src('./app/assets/images/icons/**/*.svg')
-  .pipe(svgSprite(config))
-  .pipe(gulp.dest('./app/temp/sprite'));
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest('./app/temp/sprite'));
+})
+
+gulp.task('copySpriteCss', ()=>{
+  return gulp.src('./app/temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(gulp.dest('./app/assets/styles/modules'));
 })
